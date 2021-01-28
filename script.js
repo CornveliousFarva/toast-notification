917613eb89f423cd4fae3dbf46a8fbb3db15e83e
Greg Schuman <gcschuman@hotmail.com>
2021-01-27 20:36:36 -0800

    Refactored the html, css, and script.

--- script.js
+++ script.js
@@ -12,24 +12,24 @@ const types = ['info', 'success', 'error']
 
 button.addEventListener('click', () => createNotification())
 
-function createNotification(message = null, type = null){
-   const notif = document.createElement('div')
-   notif.classList.add('toast')
-   notif.classList.add(type ? type : getRandomType())
-   
-   notif.innerText = getRandomMessage()
+function createNotification(message = null, type = null) {
+    const notif = document.createElement('div')
+    notif.classList.add('toast')
+    notif.classList.add(type ? type : getRandomType())
 
-   toasts.appendChild(notif)
+    notif.innerText = message ? message : getRandomMessage()
+
+    toasts.appendChild(notif)
 
     setTimeout(() => {
         notif.remove()
     }, 3000)
 }
 
-function getRandomMessage () {
-       return messages[Math.floor(Math.random() * messages.length)]
+function getRandomMessage() {
+    return messages[Math.floor(Math.random() * messages.length)]
 }
 
-function getRandomType () {
-    return messages[Math.floor(Math.random() * messages.length)]
+function getRandomType() {
+    return types[Math.floor(Math.random() * types.length)]
 }
